a9d2766052223d0ec5b849533b62758ea8888ae6
08priyu <166955711+08priyu@users.noreply.github.com>
2024-05-13 22:25:30 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Multiplication{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter b: ");
+        int b = sc.nextInt();
+        System.out.println("multiplication of a and b is: "+(a*b));
+
+
+
+    }
+
+}
