1e47c850e8e12f443ff3bd68836a4076b88b6df7
08priyu <166955711+08priyu@users.noreply.github.com>
2024-05-13 22:24:10 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Add{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a: ");
+        int a = sc.nextInt();
+        System.out.println("enter b: ");
+        int b = sc.nextInt();
+        System.out.println("sum of a and b is: "+(a+b));
+
+
+
+    }
+}
