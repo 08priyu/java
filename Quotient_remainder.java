a1c6f20704d355da40359279927cd41e9fd8041c
08priyu <166955711+08priyu@users.noreply.github.com>
2024-05-13 22:32:53 +0530

    Quotient_remainder.java

--- Quotient_remainder.java
+++ Quotient_remainder.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 class Quotient_remainder{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a: ");
-        int a = sc.nextInt();
-        System.out.println("Enter b: ");
-        int b =sc.nextInt();
-        int quotient = a/b;
-        int remainder = a-(quotient*b);
+        System.out.println("Enter Dividend: ");
+        int Dividend = sc.nextInt();
+        System.out.println("Enter Divisior: ");
+        int Divisior =sc.nextInt();
+        int quotient = Dividend/Divisior;
+        int remainder = Dividend-(quotient*Divisior);
         System.out.println("Quotient is: "+quotient);
         System.out.println("Remainder is: "+remainder);
 
